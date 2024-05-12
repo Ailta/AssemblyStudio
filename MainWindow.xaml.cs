a945b2033eca67e7f20bdee89d9cd7571564ca61
Karel Kříž <KrizK21@spstrutnov.cz>
2024-05-12 19:25:33 +0200

    Cleaned the code a bit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,222 +1,81 @@
-﻿using System.Diagnostics;
-using System.Text;
+﻿using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AssemblyStudio
-{
+namespace AssemblyStudio {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
+    public partial class MainWindow : Window {
         List<Label> labelsLineIndex = new List<Label>();
         private int fontSize = 15;
         private int lineHeight = 10;
-        private double[] gridSize = { 0, 0 };
-        private void setTextEditor()
-        {
-            gridSize[0] = textEditor.ActualWidth;
-            gridSize[1] = textEditor.ActualHeight;
 
-            SolidColorBrush fontColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD6D6D6"));
-            double rowHeight = fontSize+(fontSize-lineHeight);
-            int rows = textEditor.RowDefinitions.Count();
+        public MainWindow() {
+            InitializeComponent();
+        }
 
-            // Set style to a richTextBox
-            {
-                richTextBox.Padding = new Thickness(0);
-                richTextBox.Margin = new Thickness(0);
-                richTextBox.AcceptsReturn = false;
-                richTextBox.AcceptsTab = true;
-                richTextBox.TextChanged += editTextEditor;
-                richTextBox.FontSize = fontSize;
-                richTextBox.AppendText(" ");
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            TextEditor.FontSize = fontSize;
+            TextEditor.AppendText(" ");
 
-                foreach (Paragraph paragraph in richTextBox.Document.Blocks)
-                {
-                    paragraph.Margin = new Thickness(0);
-                    paragraph.Padding = new Thickness(0);
-                    paragraph.LineHeight = lineHeight;
-                }
+            foreach (Paragraph paragraph in TextEditor.Document.Blocks) {
+                paragraph.Margin = new Thickness(0);
+                paragraph.Padding = new Thickness(0);
+                paragraph.LineHeight = lineHeight;
             }
         }
 
-        private int prevLineCount = 0;
-        private void editTextEditor(object? sender, RoutedEventArgs? e)
-        {
-            TextRange textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+        private void EditTextEditor(object? sender, RoutedEventArgs? e) {
+            TextRange textRange = new TextRange(TextEditor.Document.ContentStart, TextEditor.Document.ContentEnd);
             string[] lines = textRange.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
 
-            if (prevLineCount != lines.Length - 1)
-            {
-                SolidColorBrush fontColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF666666"));
-                double rowHeight = GetLineHeightInPixels(richTextBox);
-                
-                lineIndex.Children.Clear();
-                lineIndex.RowDefinitions.Clear();
-                textBox.RowDefinitions.Clear();
-                labelsLineIndex.Clear();
+            double rowHeight = GetLineHeightInPixels(TextEditor);
 
-                for (int i = 0; i < lines.Length-1; i++)
-                {
-                    // Add row
-                    lineIndex.RowDefinitions.Add(new RowDefinition { Height = new GridLength(rowHeight, GridUnitType.Pixel) });
-                    textBox.RowDefinitions.Add(new RowDefinition { Height = new GridLength(rowHeight, GridUnitType.Pixel) });
+            LineIndexGrid.Children.Clear();
+            LineIndexGrid.RowDefinitions.Clear();
+            TextEditorHelperGrid.RowDefinitions.Clear();
+            labelsLineIndex.Clear();
 
-                    // Create and add labelIndex
-                    var label = createLabel(fontColor, rowHeight, i);
-                    lineIndex.Children.Add(label);
-                    labelsLineIndex.Add(label);
-                }
+            for (int i = 0; i < lines.Length - 1; i++) {
+                LineIndexGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(rowHeight, GridUnitType.Pixel) });
+                TextEditorHelperGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(rowHeight, GridUnitType.Pixel) });
+
+                Label label = createLabel($"{i}", fontSize, ConvertHexToSolidColorBrush("#FF666666"), [0, rowHeight], new Thickness(0), new Thickness(0));
+                Grid.SetRow(label, i);
+
+                LineIndexGrid.Children.Add(label);
+                labelsLineIndex.Add(label);
             }
         }
 
-        private void ScrollChanged(object sender, ScrollChangedEventArgs e)
-        {
-            if (sender == lineIndexScroll) { 
-                richTextBox.ScrollToVerticalOffset(e.VerticalOffset);
-                textBoxScroll.ScrollToVerticalOffset(e.VerticalOffset);
+        private void SelectionChanged(object? sender, RoutedEventArgs? e) {
+            TextEditorHelperGrid.Children.Clear();
+
+            int caretLinePos = GetCaretPosition(TextEditor);
+            double rowHeight = GetLineHeightInPixels(TextEditor);
+            TextEditorHelperGrid.Children.Add(createBorder(ConvertHexToSolidColorBrush("#FF4D4D4D"), rowHeight, caretLinePos));
+
+            EditTextEditor(null, null);
+
+            Label tmpLabel = labelsLineIndex[caretLinePos];
+            tmpLabel.Foreground = ConvertHexToSolidColorBrush("#FFD6D6D6");
+            LineIndexGrid.Children[caretLinePos] = tmpLabel;
+        }
+
+        private void ScrollChanged(object sender, ScrollChangedEventArgs e) {
+            if (sender == LineIndexScrollViewer) {
+                TextEditor.ScrollToVerticalOffset(e.VerticalOffset);
+                TextEditorHelperScrollViewer.ScrollToVerticalOffset(e.VerticalOffset);
             }
-            if (sender == richTextBox) { 
-                lineIndexScroll.ScrollToVerticalOffset(e.VerticalOffset);
-                textBoxScroll.ScrollToVerticalOffset(e.VerticalOffset);
+            if (sender == TextEditor) {
+                LineIndexScrollViewer.ScrollToVerticalOffset(e.VerticalOffset);
+                TextEditorHelperScrollViewer.ScrollToVerticalOffset(e.VerticalOffset);
             }
-        }
-
-        private double GetLineHeightInPixels(RichTextBox richTextBox)
-        {
-            FormattedText formattedText = new FormattedText(
-                "Hello World!",
-                System.Globalization.CultureInfo.CurrentCulture,
-                FlowDirection.LeftToRight,
-                new Typeface(richTextBox.FontFamily, richTextBox.FontStyle, richTextBox.FontWeight, richTextBox.FontStretch),
-                richTextBox.FontSize,
-                Brushes.Black,
-                VisualTreeHelper.GetDpi(this).PixelsPerDip);
-
-            double lineHeight = formattedText.Height;
-
-            return lineHeight;
-        }
-
-        private Label createLabel(SolidColorBrush fontColor, double rowHeight, int line)
-        {
-            Label label = new Label();
-            {
-                label.Content = line;
-                label.FontSize = fontSize;
-                label.Foreground = fontColor;
-                label.Height = rowHeight;
-                label.Padding = new Thickness(0);
-                label.Margin = new Thickness(0);
-                label.HorizontalContentAlignment = HorizontalAlignment.Center;
-                label.VerticalContentAlignment = VerticalAlignment.Center;
-                label.HorizontalAlignment = HorizontalAlignment.Stretch;
-                label.VerticalAlignment = VerticalAlignment.Stretch;
-                label.BorderBrush = null;
-                label.BorderThickness = new Thickness(1);
-                label.FontFamily = new FontFamily("Consolas");
-                //label.FontWeight = FontWeights.Black;
-
-                Grid.SetRow(label, line);
-            }
-            return label;
-        }
-
-        private Border createBorder(SolidColorBrush fontColor, double rowHeight, int line)
-        {
-            Border border = new Border();
-            border.Height = rowHeight;
-            border.BorderThickness = new Thickness(2);
-            border.BorderBrush = fontColor;
-
-            Grid.SetRow(border, line);
-
-            return border;
-        }
-
-        private void SelectionChanged(object? sender, RoutedEventArgs? e)
-        {
-            textBox.Children.Clear();
-
-            // Get the current caret position
-            TextPointer caretPos = richTextBox.CaretPosition;
-
-            // Get the starting position of the line where the caret is positioned
-            TextPointer lineStartPos = caretPos.GetLineStartPosition(0);
-
-            // Calculate the line number by counting the number of lines from the start of the document
-            int lineNumber = 0;
-            TextPointer position = richTextBox.Document.ContentStart;
-            while (position != null && position.CompareTo(lineStartPos) < 0)
-            {
-                if ((position.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.ElementStart) && (position.Parent is Paragraph))
-                {
-                    lineNumber++;
-                }
-                position = position.GetNextContextPosition(LogicalDirection.Forward);
-            }
-            SolidColorBrush fontColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF4D4D4D"));
-            double rowHeight = GetLineHeightInPixels(richTextBox);
-
-            textBox.Children.Add(createBorder(fontColor, rowHeight, lineNumber));
-
-            editTextEditor(null, null);
-
-            fontColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD6D6D6"));
-            Label tmpLabel = labelsLineIndex[lineNumber];
-            tmpLabel.Foreground = fontColor;
-            lineIndex.Children[lineNumber] = tmpLabel;
-        }
-
-        private void richTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            /*if (e.Key == Key.Enter)
-            {
-                // Create a new paragraph and insert it after the current paragraph
-                Paragraph newParagraph = new Paragraph(new Run(""));
-                newParagraph.Margin = new Thickness(0);
-                newParagraph.Padding = new Thickness(0);
-                newParagraph.LineHeight = lineHeight;
-
-                richTextBox.CaretPosition = newParagraph.ContentStart;
-
-                e.Handled = true;
-            }*/
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            setTextEditor();
-        }
-
-        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
-            Debug.WriteLine("hello?");
-        }
-
-        private void Grid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
-            Debug.WriteLine("bye?");
-        }
-
-        private void Grid_MouseEnter(object sender, MouseEventArgs e) {
-            Debug.WriteLine("f");
-        }
-
-        private void Rectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
-            Debug.WriteLine("hello?");
         }
     }
 }
---
+++ Tools.cs
@@ -0,0 +1,112 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace AssemblyStudio {
+    partial class MainWindow {
+        private int GetCaretPosition(RichTextBox richTextBox) {
+            TextPointer caretPos = richTextBox.CaretPosition;
+            TextPointer lineStartPos = caretPos.GetLineStartPosition(0);
+
+            // Calculate the line number by counting the number of lines from the start of the document
+            int lineNumber = 0;
+            TextPointer position = richTextBox.Document.ContentStart;
+            while (position != null && position.CompareTo(lineStartPos) < 0) {
+                if ((position.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.ElementStart) && (position.Parent is Paragraph)) {
+                    lineNumber++;
+                }
+                position = position.GetNextContextPosition(LogicalDirection.Forward);
+            }
+            return lineNumber;
+        }
+
+        public SolidColorBrush ConvertHexToSolidColorBrush(string hex) {
+            hex = hex.Replace("#", string.Empty);
+
+            byte a = (byte)(Convert.ToUInt32(hex.Substring(0, 2), 16));
+            byte r = (byte)(Convert.ToUInt32(hex.Substring(2, 2), 16));
+            byte g = (byte)(Convert.ToUInt32(hex.Substring(4, 2), 16));
+            byte b = (byte)(Convert.ToUInt32(hex.Substring(6, 2), 16));
+
+            SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(a, r, g, b));
+
+            return brush;
+        }
+
+        private double GetLineHeightInPixels(RichTextBox richTextBox) {
+            FormattedText formattedText = new FormattedText(
+                "Hello World!",
+                System.Globalization.CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight,
+                new Typeface(richTextBox.FontFamily, richTextBox.FontStyle, richTextBox.FontWeight, richTextBox.FontStretch),
+                richTextBox.FontSize,
+                Brushes.Black,
+                VisualTreeHelper.GetDpi(this).PixelsPerDip);
+
+            double lineHeight = formattedText.Height;
+
+            return lineHeight;
+        }
+
+        private Label createLabel(string text, int fontSize, SolidColorBrush fontColor, double[] size, Thickness padding, Thickness margin) {
+            Label label = new Label();
+
+            label.Content = text;
+            label.FontSize = fontSize;
+            label.Foreground = fontColor;
+            label.Height = size[1];
+            label.Padding = new Thickness(0);
+            label.Margin = new Thickness(0);
+            label.BorderThickness = new Thickness(0);
+            label.FontFamily = new FontFamily("Consolas");
+
+            return label;
+        }
+
+        private Border createBorder(SolidColorBrush fontColor, double rowHeight, int line) {
+            Border border = new Border();
+            border.Height = rowHeight;
+            border.BorderThickness = new Thickness(2);
+            border.BorderBrush = fontColor;
+
+            Grid.SetRow(border, line);
+
+            return border;
+        }
+
+        /*private bool windowMaximized = false;
+        private double[] sizeAndLocationSave = new double[4];
+        private void WindowActions(object sender, RoutedEventArgs e) {
+            if (sender == ExitButton) { this.Close(); } else if (sender == MaximizeButton) {
+                switch (windowMaximized) {
+                    case true:
+                        this.WindowState = WindowState.Normal;
+                        this.Width = sizeAndLocationSave[0];
+                        this.Height = sizeAndLocationSave[1];
+                        this.Left = sizeAndLocationSave[2];
+                        this.Top = sizeAndLocationSave[3];
+                        break;
+                    default:
+                        sizeAndLocationSave[0] = this.Width;
+                        sizeAndLocationSave[1] = this.Height;
+                        sizeAndLocationSave[2] = this.Left;
+                        sizeAndLocationSave[3] = this.Top;
+                        this.WindowState = WindowState.Normal;
+                        this.Width = SystemParameters.WorkArea.Width;
+                        this.Height = SystemParameters.WorkArea.Height;
+                        this.Left = 0;
+                        this.Top = 0;
+                        break;
+                }
+                windowMaximized = !windowMaximized;
+            } else if (sender == MinimizeButton) { this.WindowState = WindowState.Minimized; }
+        }*/
+
+        private void DragWindow(object sender, MouseButtonEventArgs e) {
+            bool dragWindow = (e.ButtonState == MouseButtonState.Pressed) ? true : false;
+            if (dragWindow) { this.DragMove(); }
+        }
+    }
+}
